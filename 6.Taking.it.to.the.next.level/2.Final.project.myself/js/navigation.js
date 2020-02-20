527da285140984b7d45022b8ffedadf0012e7651
Jeremie Litzler <jeremie.litzler@conduent.com>
2020-02-20 13:41:55 +0100

    feat: animate small screen nav

--- 6.Taking.it.to.the.next.level/2.Final.project.myself/js/navigation.js
+++ 6.Taking.it.to.the.next.level/2.Final.project.myself/js/navigation.js
@@ -1,14 +1,13 @@
-var openingNavClass = "opening-nav";
-var closingNavClass = "closing-nav";
+var togglingNavClass = "toggling-nav";
+
 var closeNavBtn = document.querySelector(".close-nav");
 var openNavButton = document.querySelector(".toggle-nav");
 var navigationElement = document.querySelector("nav");
 
 closeNavBtn.addEventListener("click", function(e) {
-  navigationElement.classList.remove(openingNavClass);
-  navigationElement.classList.add(closingNavClass);
+  navigationElement.classList.remove(togglingNavClass);
 });
 openNavButton.addEventListener("click", function(e) {
-  navigationElement.classList.remove(closingNavClass);
-  navigationElement.classList.add(openingNavClass);
+  navigationElement.classList.add(togglingNavClass);
+  closeNavBtn.focus();
 });
